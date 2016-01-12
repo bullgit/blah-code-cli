7347621b3109fad5ef05f8cbff9aa15f5364cebd
Michael Kühnel <m.kuehnel@micromata.de>
2016-01-12 23:15:19 +0100

    Add tests

--- test.js
+++ test.js
@@ -1,6 +1,32 @@
 import test from 'ava';
-import fn from './';
+import childProcess from 'child_process';
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+test.cb(t => {
+	childProcess.execFile('./cli.js', {
+		cwd: __dirname
+	}, (err, stdout) => {
+		t.ifError(err);
+		t.is(stdout.trim(), 'I need input. Enter `blah-code --help` if you need assistance`');
+		t.end();
+	});
+});
+
+test.cb(t => {
+	childProcess.execFile('./cli.js', ['Hi!'], {
+		cwd: __dirname
+	}, (err, stdout) => {
+		t.ifError(err);
+		t.is(stdout.trim(), 'blah blah blah blah blah blah blah blah blah, blah blah blah blah blah blah blah blah blah blah, blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah');
+		t.end();
+	});
+});
+
+test.cb(t => {
+	childProcess.execFile('./cli.js', ['blah blah blah blah blah blah blah blah blah, blah blah blah blah blah blah blah blah blah blah, blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah', '--decode'], {
+		cwd: __dirname
+	}, (err, stdout) => {
+		t.ifError(err);
+		t.is(stdout.trim(), 'hi!');
+		t.end();
+	});
 });
