08320f277fcc23cbda2ff8d5938b2380bff40a2e
Michael Kühnel <m.kuehnel@micromata.de>
2016-01-12 22:37:46 +0100

    Implement encoding and decoding blah code

--- cli.js
+++ cli.js
@@ -1,20 +1,35 @@
 #!/usr/bin/env node
 'use strict';
 var meow = require('meow');
-var blahCodeCli = require('./');
+var blahCode = require('blah-code');
 
 var cli = meow([
 	'Usage',
-	'  $ blah-code-cli [input]',
+	'  $ blah-code [input]',
 	'',
 	'Options',
-	'  --foo  Lorem ipsum. [Default: false]',
+	'  --encode, -e  Returns the blah code representation of the given text input [Default: true]',
+	'  --decode, -e  Returns regular text of the given blah code [Default: false]',
+	'  --help,   -h  Shows the help',
 	'',
 	'Examples',
-	'  $ blah-code-cli',
-	'  unicorns & rainbows',
-	'  $ blah-code-cli ponies',
-	'  ponies & rainbows'
-]);
+	'  $ blah-code Hi!',
+	'  blah blah blah blah blah blah blah blah blah, blah blah blah blah blah blah blah blah blah blah, blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+	'',
+	'  $ blah-code --decode "blah blah blah blah blah blah blah blah blah, blah blah blah blah blah blah blah blah blah blah, blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah"',
+	'  hi!'
+], {
+	alias: {
+		h: 'help',
+		e: 'encode',
+		d: 'decode'
+	}
+});
 
-console.log(blahCodeCli(cli.input[0] || 'unicorns'));
+if (cli.input[0] === undefined) {
+	console.log('I need input. Enter `blah-code --help` if you need assistance`');
+} else if (cli.flags.decode) {
+	console.log(blahCode.decode(cli.input[0]));
+} else {
+	console.log(blahCode.encode(cli.input[0]));
+}
